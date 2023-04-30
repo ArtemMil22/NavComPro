6661e8ee4ec7de9928b5d44af679bf1812332fd2
ArtemMil22 <bullfinch22@yandex.ru>
2023-04-30 17:20:12 +0400

    Add tabs structure

--- app/src/main/java/com/example/navcompro/screens/main/toobs/TabsFragment.kt
+++ app/src/main/java/com/example/navcompro/screens/main/toobs/TabsFragment.kt
@@ -3,6 +3,8 @@ package com.example.navcompro.screens.main.toobs
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.navcompro.R
 
 import com.example.navcompro.databinding.FragmentTabsBinding
@@ -14,6 +16,12 @@ class TabsFragment : Fragment(R.layout.fragment_tabs) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTabsBinding.bind(view)
+
+        val navHost = childFragmentManager.findFragmentById(R.id.tabsContainer) as NavHostFragment
+        val navController = navHost.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
+
 
         TODO("Connect Nav Component to the BottomNavigationView here")
     }
