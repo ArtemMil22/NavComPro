1ea8b2104b03d6803a029230a5ad1d131185b278
ArtemMil22 <bullfinch22@yandex.ru>
2023-05-03 02:00:09 +0400

    2. Account entity implementation (AccountDbEntity).

--- app/src/main/java/com/example/navcompro/model/accounts/room/entities/AccountDbEntity.kt
+++ app/src/main/java/com/example/navcompro/model/accounts/room/entities/AccountDbEntity.kt
@@ -1,27 +1,45 @@
 package com.example.navcompro.model.accounts.room.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.example.navcompro.model.accounts.entities.Account
 import com.example.navcompro.model.accounts.entities.SignUpData
 
+@Entity(
+    tableName = "accounts",
+    indices = [
+        Index("email", unique = true)
+    ]
+)
+ data class AccountDbEntity(
+    @PrimaryKey (autoGenerate = true) val id: Long,
+    @ColumnInfo (collate = ColumnInfo.NOCASE) val email: String,
+    val username: String,
+    val password: String,
+    @ColumnInfo (name = "create_at") val createdAt: Long
+ ) {
 
-// todo #5: Define AccountDbEntity.
-//          - fields: id, email, username, password, createdAt.
-//          - annotate class with @Entity annotation:
-//            - use 'indices' parameter to add UNIQUE constraint for 'email' field;
-//            - use 'tableName' to customize the table name for this entity;
-//          - use @PrimaryKey annotation for the identifier field:
-//            - use 'autoGenerate' parameter if you don't want to specify ID every time upon creating a new account;
-//          - use @ColumnInfo(name='...') to customize column names and add additional
-//            parameters (e.g. 'collate = ColumnInfo.NOCASE').
-//          - add toAccount() method for mapping AccountDbEntity objects to Account objects.
-//          - add companion object with fromSignUpData(signUpData: SignUpData) method for creating a new
-//            instance of AccountDbEntity from SignUpData object.
-class AccountDbEntity {
+    // маперы для быстрого преобразования данные сущности из табл
+    // в стандартный аккаунт апп для публичного использования
+    fun toAccount(): Account = Account(
+        id = id,
+        email = email,
+        username = username,
+        createdAt = createdAt
+    )
 
-    fun toAccount(): Account = TODO()
-
+    //обратное преобразование из заполненного бланка регистрации
+    // изначально данные попав в SingUpData, должно преобразоваться в сущность БД
     companion object {
-        fun fromSignUpData(signUpData: SignUpData): AccountDbEntity = TODO()
+        fun fromSignUpData(signUpData: SignUpData): AccountDbEntity = AccountDbEntity(
+            id = 0,
+            email = signUpData.email,
+            username = signUpData.username,
+            password = signUpData.password,
+            createdAt = System.currentTimeMillis()
+        )
     }
 
 }
