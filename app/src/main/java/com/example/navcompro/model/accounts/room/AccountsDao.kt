9a1315b3dfa630e7d54187999760257ed526655b
ArtemMil22 <bullfinch22@yandex.ru>
2023-05-04 04:41:36 +0400

    5.  Created fun from AccountsDao add  in @DataBase and create fun  implementation Dao

--- app/src/main/java/com/example/navcompro/model/accounts/room/AccountsDao.kt
+++ app/src/main/java/com/example/navcompro/model/accounts/room/AccountsDao.kt
@@ -1,16 +1,26 @@
 package com.example.navcompro.model.accounts.room
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import com.example.navcompro.model.accounts.room.entities.AccountDbEntity
 import com.example.navcompro.model.accounts.room.entities.AccountSignInTuple
 import com.example.navcompro.model.accounts.room.entities.AccountUpdateUsernameTuple
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AccountsDao {
 
     @Query("SELECT id,password FROM accounts WHERE email = :email")
-    suspend fun findByEmail(email: String):AccountSignInTuple?
+    suspend fun findByEmail(email: String): AccountSignInTuple?
 
+    @Update(entity = AccountDbEntity::class)
     suspend fun updateUserName(accountUpdateUsernameTuple: AccountUpdateUsernameTuple)
 
+    @Insert
+    suspend fun createAccount(accountDbEntity: AccountDbEntity)
+
+    @Query("SELECT * FROM accounts WHERE id = :accountId")
+    fun getById(accountId: Long): Flow<AccountDbEntity?>
 }
--- app/src/main/java/com/example/navcompro/model/room/AppDatabase.kt
+++ app/src/main/java/com/example/navcompro/model/room/AppDatabase.kt
@@ -2,6 +2,7 @@ package com.example.navcompro.model.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.navcompro.model.accounts.room.AccountsDao
 import com.example.navcompro.model.accounts.room.entities.AccountDbEntity
 
 //  Create a database class by extending RoomDatabase.
@@ -14,8 +15,6 @@ import com.example.navcompro.model.accounts.room.entities.AccountDbEntity
 )
 abstract class AppDatabase : RoomDatabase() {
 
-    // todo #10: Add abstract getAccountsDao() method
-
-    // todo #18: Add abstract getBoxesDao() method
+    abstract fun getAccountsDao(): AccountsDao
 
 }
